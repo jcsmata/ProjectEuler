6c0ce7ae76f21db6b0b42beb700064be1f2d6987
João Mata <joao.mata@ist.utl.pt>
2014-12-29 18:46:54 +0000

    Created class for problem 004

---
+++ src/euler/project/problem004/Problem.java
@@ -0,0 +1,15 @@
+package euler.project.problem004;
+
+public class Problem {
+	
+/**
+ * A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+ * Find the largest palindrome made from the product of two 3-digit numbers.
+ */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
